de1acf14aa5ade15f2e3ca8f50ec94eb28a0894f
Derek Chen <derek.g.chen@gmail.com>
2018-05-07 23:24:00 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -15,12 +15,14 @@ class MP3Importer
   end
 
   def files
+
+    
      a = File.basename(path, ".mp3") # => "xyz"
     path_ = path + "/**/*.mp3"
     path2 = path + "/**/*"
     b =Dir[path_].map{ |f| File.basename(f,".mp3") }
     d =Dir[path2].map{ |f| File.basename f }
-     c = Dir[path_].select{ |f| File.file? f }.map{ |f| File.basename f }
+    c = Dir[path_].select{ |f| File.file? f }.map{ |f| File.basename f }
 
     binding.pry
   end
@@ -34,4 +36,4 @@ class MP3Importer
 
 end
 
-MP3Importer.new('./db/test').files
+MP3Importer.new('./db/test/').files
