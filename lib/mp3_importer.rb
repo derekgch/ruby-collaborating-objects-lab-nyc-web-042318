5cc0b38e1ff2dad96d4bdbe3b21996b3a2aac6e4
Derek Chen <derek.g.chen@gmail.com>
2018-05-07 23:23:40 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,8 @@
+# require_relative "artist.rb"
+# require_relative "song.rb"
+# require_relative "../lib/mp3_importer.rb"
 
+require "pry"
 
 #MP3Importer.new('./db/mp3s').import
 
@@ -11,19 +15,23 @@ class MP3Importer
   end
 
   def files
-    # a = File.basename(path, ".mp3") # => "xyz"
+     a = File.basename(path, ".mp3") # => "xyz"
     path_ = path + "/**/*.mp3"
-    b =Dir[path_].map{ |f| File.basename f }
-    # c = Dir[path].select{ |f| File.file? f }.map{ |f| File.basename f }
+    path2 = path + "/**/*"
+    b =Dir[path_].map{ |f| File.basename(f,".mp3") }
+    d =Dir[path2].map{ |f| File.basename f }
+     c = Dir[path_].select{ |f| File.file? f }.map{ |f| File.basename f }
 
-    # binding.pry
+    binding.pry
   end
 
-  def import
-    self.files.each do |v|
-      Song.new_by_filename(v)
-    end
-
-  end
+  # def import
+  #   self.files.each do |v|
+  #     Song.new_by_filename(v)
+  #   end
+  #
+  # end
 
 end
+
+MP3Importer.new('./db/test').files
