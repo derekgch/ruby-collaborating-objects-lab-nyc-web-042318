404b68abcfed9531da217fe3a07efa034e40158b
Derek Chen <derek.g.chen@gmail.com>
2018-05-08 00:02:18 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -23,6 +23,7 @@ class MP3Importer
     b =Dir[path_].map{ |f| File.basename(f,".mp3") }
     d =Dir[path2].map{ |f| File.basename f }
     c = Dir[path_].select{ |f| File.file? f }.map{ |f| File.basename f }
+    f = Dir[path_].select{ |f| File.extname == ".mp3" }.map{ |f| File.basename f } 
 
     binding.pry
   end
